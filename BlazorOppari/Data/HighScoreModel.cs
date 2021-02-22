111dc2210f0da7c55ffca7a6f959a4681dfa954b
ollinok <62589655+ollinok@users.noreply.github.com>
2021-02-23 01:57:22 +0200

    New High score functionality

---
+++ BlazorOppari/Data/HighScoreModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOppari.Data
+{
+    public class HighScoreModel
+    {
+        public int Id { get; set; }
+        public double Time { get; set; }
+        public DateTime Datetime { get; set; }
+        public string Username { get; set; }
+    }
+}
--- DatabaseLib/typing.sql
+++ DatabaseLib/typing.sql
@@ -1,15 +1,28 @@
 CREATE DATABASE typinggame;
 
+SET time_zone = 'Europe/Helsinki';
+
 USE typinggame;
 
 CREATE TABLE word (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	length INT NOT NULL,
-	typed TEXT NOT NULL
+	"id" INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	"length" INT NOT NULL,
+	"typed" TEXT NOT NULL
 );
 
 INSERT IGNORE INTO word (length, typed)
 VALUES 
 (4, "home"),(4, "move"),(4, "luck"),(4, "bass"),(4, "love"),(4, "harm"),
 (5, "pizza"),(5, "icons"),(5, "labor"),(5, "gauge"),(5, "cable"),(5, "large"),
-(6, "cactus"),(6, "iconic"),(6, "ladder"),(6, "abduct"),(6, "easily"),(6, "income"); 
+(6, "cactus"),(6, "iconic"),(6, "ladder"),(6, "abduct"),(6, "easily"),(6, "income");
+
+CREATE TABLE highscore (
+	"id" INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	"time" DECIMAL(5,3) NOT NULL,
+	"datetime" DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	"username" VARCHAR(8) NOT NULL
+);
+
+INSERT IGNORE INTO highscore (time, username)
+VALUES
+(21.231, "Pertti"),(20.100, "Jorma");
